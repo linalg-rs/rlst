8bbc70ca853abd3f999ff488d814fdd702815ecb
Timo Betcke <timo.betcke@gmail.com>
2023-05-19 12:16:33 +0100

    Pretty Printer for matrices

--- common/src/tools.rs
+++ common/src/tools.rs
@@ -1,6 +1,9 @@
 //! Useful library tools.
 
-use crate::types::*;
+use crate::{
+    traits::{RandomAccessByValue, Shape},
+    types::*,
+};
 use rand::prelude::*;
 use rand_distr::Distribution;
 
@@ -51,4 +54,158 @@ impl RandScalar for c64 {
     ) -> Self {
         c64::new(dist.sample(rng), dist.sample(rng))
     }
+}
+
+// pub struct MatrixPrettyPrinter<'a, T: Scalar, Mat: RandomAccessByValue<Item = T> + Shape> {
+//     mat: &'a Mat,
+//     print_width: usize,
+//     mantissa: usize,
+//     exponent: usize,
+//     _phantom: std::marker::PhantomData<T>,
+// }
+
+// impl<'a, T: Scalar, Mat: RandomAccessByValue<Item = T> + Shape> MatrixPrettyPrinter<'a, T, Mat> {
+//     pub fn new(mat: &'a Mat, print_width: usize, mantissa: usize, exponent: usize) -> Self {
+//         Self {
+//             mat,
+//             print_width,
+//             mantissa,
+//             exponent,
+//             _phantom: std::marker::PhantomData,
+//         }
+//     }
+// }
+
+// pub trait ToPrettyPrinter {
+//     type T: Scalar;
+//     type Out<'a>
+//     where
+//         Self: 'a;
+
+//     fn pretty_print<'a>(&'a self) -> Self::Out<'a>;
+
+//     fn pretty_print_with_parameters<'a>(
+//         &'a self,
+//         print_width: usize,
+//         mantissa: usize,
+//         exponent: usize,
+//     ) -> Self::Out<'a>;
+// }
+
+pub trait PrettyPrint<T: Scalar> {
+    fn pretty_print(&self);
+    fn pretty_print_with_dimension(&self, rows: usize, cols: usize);
+    fn pretty_print_advanced(
+        &self,
+        rows: usize,
+        cols: usize,
+        print_width: usize,
+        mantissa: usize,
+        exponent: usize,
+    );
+}
+
+macro_rules! pretty_print_impl {
+    ($scalar:ty, $fmtfun:ident) => {
+        impl<Mat: RandomAccessByValue<Item = $scalar> + Shape> PrettyPrint<$scalar> for Mat {
+            fn pretty_print(&self) {
+                self.pretty_print_advanced(self.shape().0, self.shape().1, 11, 3, 2)
+            }
+
+            fn pretty_print_with_dimension(&self, rows: usize, cols: usize) {
+                self.pretty_print_advanced(rows, cols, 11, 3, 2)
+            }
+
+            fn pretty_print_advanced(
+                &self,
+                rows: usize,
+                cols: usize,
+                print_width: usize,
+                mantissa: usize,
+                exponent: usize,
+            ) {
+                let shape = self.shape();
+                let shape = (std::cmp::min(shape.0, rows), std::cmp::min(shape.1, cols));
+                let mut content_str = String::new();
+
+                // For alignment in columns, must satisfy: 4 + mantissa + exponent < print_width
+                let print_width = std::cmp::max(print_width, 5 + mantissa + exponent);
+
+                let num_outer_spaces = print_width - mantissa - exponent - 4;
+
+                let mut x_ij = self.get_value(0, 0).unwrap();
+                for row in 0..shape.0 {
+                    content_str += "│";
+                    for col in 0..shape.1 {
+                        x_ij = self.get_value(row, col).unwrap();
+
+                        content_str += &format!(
+                            " {}",
+                            $fmtfun::<$scalar>(x_ij, print_width, mantissa, exponent)
+                        );
+                    }
+                    content_str += &" ".repeat(num_outer_spaces);
+                    content_str += "│\n";
+                }
+
+                let colwidth = $fmtfun::<$scalar>(x_ij, print_width, mantissa, exponent)
+                    .chars()
+                    .count();
+                let top_str = format!(
+                    "\n┌{}┐\n",
+                    " ".repeat((shape.1 * (colwidth + 1)) + num_outer_spaces)
+                );
+                let btm_str = format!(
+                    "└{}┘\n",
+                    " ".repeat((shape.1 * (colwidth + 1)) + num_outer_spaces)
+                );
+                println!("{top_str}{content_str}{btm_str}");
+            }
+        }
+    };
+}
+
+pretty_print_impl!(f64, fmt_real);
+pretty_print_impl!(f32, fmt_real);
+pretty_print_impl!(c32, fmt_complex);
+pretty_print_impl!(c64, fmt_complex);
+
+fn fmt_real<T: Scalar>(num: T::Real, width: usize, precision: usize, exp_pad: usize) -> String {
+    let mut num = format!("{:.precision$e}", num, precision = precision);
+    // Safe to `unwrap` as `num` is guaranteed to contain `'e'`
+    #[allow(clippy::unwrap_used)]
+    let exp = num.split_off(num.find('e').unwrap());
+
+    let (sign, exp) = exp
+        .strip_prefix("e-")
+        .map_or_else(|| ('+', &exp[1..]), |stripped| ('-', stripped));
+
+    num.push_str(&format!("e{}{:0>pad$}", sign, exp, pad = exp_pad));
+
+    format!("{:>width$}", num, width = width)
+}
+
+fn fmt_complex<T: Scalar>(num: T, width: usize, precision: usize, exp_pad: usize) -> String {
+    let sign = if num.im() < <T::Real as num::Zero>::zero() {
+        "-"
+    } else {
+        "+"
+    };
+    let mut printstr = format!(
+        "{}{}",
+        fmt_real::<T::Real>(num.re(), width, precision, exp_pad),
+        fmt_real::<T::Real>(num.im().abs(), width, precision, exp_pad),
+    );
+    let num_blanks = width - precision - exp_pad - 4;
+    if num_blanks % 2 == 0 {
+        printstr = format!(
+            "{} {}𝑖",
+            fmt_real::<T::Real>(num.re(), width, precision, exp_pad),
+            fmt_real::<T::Real>(num.im().abs(), width, precision, exp_pad),
+        );
+    }
+    let sign_position = width + (num_blanks / 2);
+
+    printstr.replace_range(sign_position..=sign_position, sign);
+    printstr
 }
