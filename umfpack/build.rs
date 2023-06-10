1ad3708ca29f8f8a43ebbb8579b65d4484fa080f
Timo Betcke <timo.betcke@gmail.com>
2023-06-10 22:23:29 +0100

    Work on math.h issue for bindings.rs

--- umfpack/build.rs
+++ umfpack/build.rs
@@ -1,5 +1,52 @@
-use bindgen;
-use std::path::PathBuf;
+use bindgen::{
+    self,
+    callbacks::{MacroParsingBehavior, ParseCallbacks},
+};
+use std::{collections::HashSet, path::PathBuf};
+
+// From https://github.com/rust-lang/rust-bindgen/issues/984
+
+const IGNORE_MACROS: [&str; 20] = [
+    "FE_DIVBYZERO",
+    "FE_DOWNWARD",
+    "FE_INEXACT",
+    "FE_INVALID",
+    "FE_OVERFLOW",
+    "FE_TONEAREST",
+    "FE_TOWARDZERO",
+    "FE_UNDERFLOW",
+    "FE_UPWARD",
+    "FP_INFINITE",
+    "FP_INT_DOWNWARD",
+    "FP_INT_TONEAREST",
+    "FP_INT_TONEARESTFROMZERO",
+    "FP_INT_TOWARDZERO",
+    "FP_INT_UPWARD",
+    "FP_NAN",
+    "FP_NORMAL",
+    "FP_SUBNORMAL",
+    "FP_ZERO",
+    "IPPORT_RESERVED",
+];
+
+#[derive(Debug)]
+struct IgnoreMacros(HashSet<String>);
+
+impl ParseCallbacks for IgnoreMacros {
+    fn will_parse_macro(&self, name: &str) -> MacroParsingBehavior {
+        if self.0.contains(name) {
+            MacroParsingBehavior::Ignore
+        } else {
+            MacroParsingBehavior::Default
+        }
+    }
+}
+
+impl IgnoreMacros {
+    fn new() -> Self {
+        Self(IGNORE_MACROS.into_iter().map(|s| s.to_owned()).collect())
+    }
+}
 
 fn main() {
     let root = PathBuf::from(std::env::var("DEP_SUITESPARSE_ROOT").unwrap());
@@ -8,18 +55,14 @@ fn main() {
         // The input header we would like to generate
         // bindings for.
         .header("src/wrapper.h")
-        // Need to block some types in math.h that cause problems with bindgen
-        // on Linux. See https://github.com/rust-lang/rust-bindgen/issues/984
-        .blocklist_type("FP_NAN")
-        .blocklist_type("FP_INFINITE")
-        .blocklist_type("FP_ZERO")
-        .blocklist_type("FP_SUBNORMAL")
-        .blocklist_type("FP_NORMAL")
         // Add an include path
         .clang_arg(format!("-I{}", root.join("include").display()))
         // Tell cargo to invalidate the built crate whenever any of the
         // included header files changed.
         .parse_callbacks(Box::new(bindgen::CargoCallbacks))
+        // Need to block some types in math.h that cause problems with bindgen
+        // on Linux. See https://github.com/rust-lang/rust-bindgen/issues/984
+        .parse_callbacks(Box::new(IgnoreMacros::new()))
         // Finish the builder and generate the bindings.
         .generate()
         // Unwrap the Result and panic on failure.
