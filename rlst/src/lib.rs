8a438e10c20232769aa6c36c90d1b4d8607bf7fe
Timo Betcke <timo.betcke@gmail.com>
2023-06-28 13:31:04 +0100

    First interface version (#37)

---
+++ rlst/src/lib.rs
@@ -0,0 +1,9 @@
+//! Interface to the rlst library
+
+pub use rlst_algorithms as algorithms;
+pub use rlst_blis as blis;
+pub use rlst_common as common;
+pub use rlst_dense as dense;
+pub use rlst_operator as operator;
+pub use rlst_sparse as sparse;
+pub use rlst_umfpack as umfpack;
