ad1fa61d825455c50c15ceb3a77fe1465b373df3
Timo Betcke <timo.betcke@gmail.com>
2023-12-03 14:23:35 +0000

    WIP: Reduce amount of traits and refactor.

--- dense/src/array/operations.rs
+++ dense/src/array/operations.rs
@@ -8,121 +8,6 @@ impl<
         Item: Scalar,
         ArrayImpl: UnsafeRandomAccessByValue<NDIM, Item = Item>
             + Shape<NDIM>
-            + UnsafeRandomAccessByRef<NDIM, Item = Item>
-            + UnsafeRandomAccessMut<NDIM, Item = Item>,
-        Other: UnsafeRandomAccessByValue<NDIM, Item = Item> + Shape<NDIM> + DefaultIterator<Item = Item>,
-        const NDIM: usize,
-    > FillFrom<Other> for Array<Item, ArrayImpl, NDIM>
-{
-    fn fill_from(&mut self, other: Other) {
-        assert_eq!(self.shape(), other.shape());
-
-        for (item, other_item) in self.iter_mut().zip(other.iter()) {
-            *item = other_item;
-        }
-    }
-}
-
-impl<
-        Item: Scalar,
-        ArrayImpl: UnsafeRandomAccessByValue<NDIM, Item = Item>
-            + Shape<NDIM>
-            + UnsafeRandomAccessMut<NDIM, Item = Item>,
-        const NDIM: usize,
-    > Array<Item, ArrayImpl, NDIM>
-{
-    pub fn fill_from_chunked<
-        Other: UnsafeRandomAccessByValue<NDIM, Item = Item> + Shape<NDIM> + ChunkedAccess<N, Item = Item>,
-        const N: usize,
-    >(
-        &mut self,
-        other: Other,
-    ) {
-        assert_eq!(self.shape(), other.shape());
-
-        let mut chunk_index = 0;
-
-        while let Some(chunk) = other.get_chunk(chunk_index) {
-            let data_start = chunk.start_index;
-
-            for data_index in 0..chunk.valid_entries {
-                unsafe {
-                    *self.get_unchecked_mut(convert_1d_nd_from_shape(
-                        data_start + data_index,
-                        self.shape(),
-                    )) = chunk.data[data_index];
-                }
-            }
-            chunk_index += 1;
-        }
-    }
-}
-
-impl<
-        Item: Scalar,
-        ArrayImpl: UnsafeRandomAccessByValue<NDIM, Item = Item>
-            + Shape<NDIM>
-            + UnsafeRandomAccessByRef<NDIM, Item = Item>
-            + UnsafeRandomAccessMut<NDIM, Item = Item>,
-        Other: UnsafeRandomAccessByValue<NDIM, Item = Item> + Shape<NDIM> + DefaultIterator<Item = Item>,
-        const NDIM: usize,
-    > SumInto<Other> for Array<Item, ArrayImpl, NDIM>
-{
-    fn sum_into(&mut self, other: Other) {
-        for (item, other_item) in self.iter_mut().zip(other.iter()) {
-            *item += other_item;
-        }
-    }
-}
-
-impl<
-        Item: Scalar,
-        ArrayImpl: UnsafeRandomAccessByValue<NDIM, Item = Item>
-            + Shape<NDIM>
-            + UnsafeRandomAccessMut<NDIM, Item = Item>,
-        const NDIM: usize,
-    > Array<Item, ArrayImpl, NDIM>
-{
-    pub fn sum_into_chunked<
-        Other: UnsafeRandomAccessByValue<NDIM, Item = Item> + Shape<NDIM> + ChunkedAccess<N, Item = Item>,
-        const N: usize,
-    >(
-        &mut self,
-        other: Other,
-    ) where
-        Self: ChunkedAccess<N, Item = Item>,
-    {
-        assert_eq!(self.shape(), other.shape());
-
-        let mut chunk_index = 0;
-
-        while let (Some(mut my_chunk), Some(chunk)) =
-            (self.get_chunk(chunk_index), other.get_chunk(chunk_index))
-        {
-            let data_start = chunk.start_index;
-
-            for data_index in 0..chunk.valid_entries {
-                my_chunk.data[data_index] += chunk.data[data_index];
-            }
-
-            for data_index in 0..chunk.valid_entries {
-                unsafe {
-                    *self.get_unchecked_mut(convert_1d_nd_from_shape(
-                        data_index + data_start,
-                        self.shape(),
-                    )) = my_chunk.data[data_index];
-                }
-            }
-
-            chunk_index += 1;
-        }
-    }
-}
-
-impl<
-        Item: Scalar,
-        ArrayImpl: UnsafeRandomAccessByValue<NDIM, Item = Item>
-            + Shape<NDIM>
             + UnsafeRandomAccessMut<NDIM, Item = Item>,
         const NDIM: usize,
     > Array<Item, ArrayImpl, NDIM>
@@ -150,6 +35,87 @@ impl<
     pub fn scale_in_place(&mut self, alpha: Item) {
         for elem in self.iter_mut() {
             *elem *= alpha;
+        }
+    }
+
+    pub fn fill_from<ArrayImplOther: UnsafeRandomAccessByValue<NDIM, Item = Item> + Shape<NDIM>>(
+        &mut self,
+        other: Array<Item, ArrayImplOther, NDIM>,
+    ) {
+        assert_eq!(self.shape(), other.shape());
+
+        for (item, other_item) in self.iter_mut().zip(other.iter()) {
+            *item = other_item;
+        }
+    }
+
+    pub fn fill_from_chunked<
+        Other: UnsafeRandomAccessByValue<NDIM, Item = Item> + Shape<NDIM> + ChunkedAccess<N, Item = Item>,
+        const N: usize,
+    >(
+        &mut self,
+        other: Other,
+    ) {
+        assert_eq!(self.shape(), other.shape());
+
+        let mut chunk_index = 0;
+
+        while let Some(chunk) = other.get_chunk(chunk_index) {
+            let data_start = chunk.start_index;
+
+            for data_index in 0..chunk.valid_entries {
+                unsafe {
+                    *self.get_unchecked_mut(convert_1d_nd_from_shape(
+                        data_start + data_index,
+                        self.shape(),
+                    )) = chunk.data[data_index];
+                }
+            }
+            chunk_index += 1;
+        }
+    }
+
+    pub fn sum_into<ArrayImplOther: UnsafeRandomAccessByValue<NDIM, Item = Item> + Shape<NDIM>>(
+        &mut self,
+        other: Array<Item, ArrayImplOther, NDIM>,
+    ) {
+        for (item, other_item) in self.iter_mut().zip(other.iter()) {
+            *item += other_item;
+        }
+    }
+
+    pub fn sum_into_chunked<
+        ArrayImplOther: UnsafeRandomAccessByValue<NDIM, Item = Item> + Shape<NDIM> + ChunkedAccess<N, Item = Item>,
+        const N: usize,
+    >(
+        &mut self,
+        other: Array<Item, ArrayImplOther, NDIM>,
+    ) where
+        Self: ChunkedAccess<N, Item = Item>,
+    {
+        assert_eq!(self.shape(), other.shape());
+
+        let mut chunk_index = 0;
+
+        while let (Some(mut my_chunk), Some(chunk)) =
+            (self.get_chunk(chunk_index), other.get_chunk(chunk_index))
+        {
+            let data_start = chunk.start_index;
+
+            for data_index in 0..chunk.valid_entries {
+                my_chunk.data[data_index] += chunk.data[data_index];
+            }
+
+            for data_index in 0..chunk.valid_entries {
+                unsafe {
+                    *self.get_unchecked_mut(convert_1d_nd_from_shape(
+                        data_index + data_start,
+                        self.shape(),
+                    )) = my_chunk.data[data_index];
+                }
+            }
+
+            chunk_index += 1;
         }
     }
 }
