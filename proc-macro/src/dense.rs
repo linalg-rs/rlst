023b20b467148a21f25e1791bb50dc0705e1342e
Timo Betcke <timo.betcke@gmail.com>
2024-02-17 23:26:49 +0000

    Added proc macro for static array typedef

--- proc-macro/src/dense.rs
+++ proc-macro/src/dense.rs
@@ -62,3 +62,26 @@ pub(crate) fn rlst_static_array_impl(items: TokenStream) -> TokenStream {
 
     output.into()
 }
+
+pub(crate) fn rlst_static_type_impl(items: TokenStream) -> TokenStream {
+    let args = items.to_string();
+    let args: Vec<&str> = args.split(',').map(|elem| elem.trim()).collect();
+    let ty = args[0];
+    let ty = if let Ok(syn::Type::Path(type_path)) = syn::parse_str(ty) {
+        type_path
+    } else {
+        panic!("Type expected as first argument.");
+    };
+    let dims = args[1..]
+        .iter()
+        .map(|elem| elem.parse::<usize>().unwrap())
+        .collect::<Vec<usize>>();
+
+    let nelem: usize = dims.iter().product();
+    let ndim = args[1..].len();
+
+    let output = quote! { rlst_dense::array::Array<#ty, rlst_dense::base_array::BaseArray<#ty, rlst_dense::data_container::ArrayContainer<#ty, #nelem>, #ndim>, #ndim>
+    };
+
+    output.into()
+}
