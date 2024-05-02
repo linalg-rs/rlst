7543b0543306e157b6d8f833b9a42e45a7be872b
Timo Betcke <timo.betcke@gmail.com>
2024-05-02 10:27:57 +0100

    started op algebra doc

---
+++ src/doc/operator_algebra.rs
@@ -0,0 +1,4 @@
+//! Operator Algebra
+//!
+//! RLST supports a generic operator algebra based on the concept of function spaces and linear
+//! operators acting on them. This allows the expression of
