6cd3b2f15fca79b74acd19103832ed0940d524ce
Timo Betcke <timo.betcke@gmail.com>
2024-07-08 23:37:27 +0100

    WIP: Adding exp

--- sleef_interface/sleef.c
+++ sleef_interface/sleef.c
@@ -5,18 +5,26 @@
 typedef struct {
   double x[2];
   double y[2];
-} float64x2x2;
+} rlst_float64x2x2;
 
 typedef struct {
   float x[4];
   float y[4];
-} float32x4x2;
+} rlst_float32x4x2;
 
-float64x2x2 sin_cos_f64(double value[2] ) {
+typedef struct {
+  float x[4];
+} rlst_float32x4;
+
+typedef struct {
+  double x[2];
+} rlst_float64x2;
+
+rlst_float64x2x2 rlst_neon_sin_cos_f64(double* value) {
 
   float64x2_t simd_value = vld1q_f64(value);
   Sleef_float64x2_t_2 simd_output = Sleef_sincosd2_u10advsimd(simd_value);
-  float64x2x2 output;
+  rlst_float64x2x2 output;
 
 
   vst1q_f64(output.x, simd_output.x);
@@ -25,11 +33,11 @@ float64x2x2 sin_cos_f64(double value[2] ) {
   return output;
 }
 
-float32x4x2 sin_cos_f32(float value[4] ) {
+rlst_float32x4x2 rlst_neon_sin_cos_f32(float* value ) {
 
   float32x4_t simd_value = vld1q_f32(value);
   Sleef_float32x4_t_2 simd_output = Sleef_sincosf4_u10advsimd(simd_value);
-  float32x4x2 output;
+  rlst_float32x4x2 output;
 
 
   vst1q_f32(output.x, simd_output.x);
@@ -38,3 +46,14 @@ float32x4x2 sin_cos_f32(float value[4] ) {
   return output;
 }
 
+rlst_float64x2 rlst_neon_exp_f64(double* value) {
+
+  float64x2_t simd_value = vld1q_f64(value);
+  float64x2_t simd_output = Sleef_expd2_u10advsimd(simd_value);
+  rlst_float64x2 output;
+
+
+  vst1q_f64(output.x, simd_output);
+
+  
+}
