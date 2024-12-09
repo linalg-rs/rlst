a1ec98ab9cdcac77f3c3e876f0f27709e6981386
Timo Betcke <timo.betcke@gmail.com>
2024-12-09 16:01:32 +0000

    Fixed warnings

--- src/sparse/sparse_mat/distributed_csr_mat.rs
+++ src/sparse/sparse_mat/distributed_csr_mat.rs
@@ -6,7 +6,6 @@ use crate::sparse::index_layout::DefaultDistributedIndexLayout;
 use crate::sparse::sparse_mat::csr_mat::CsrMatrix;
 use crate::sparse::sparse_mat::SparseMatType;
 use crate::sparse::traits::index_layout::IndexLayout;
-use crate::RlstResult;
 use bempp_ghost::GhostCommunicator;
 use itertools::{izip, Itertools};
 
