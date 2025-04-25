4b3b81175d23aaee85e50a7416c30b99c0e6e5ac
Timo Betcke <timo.betcke@gmail.com>
2025-04-26 00:12:25 +0100

    WIP: Fixing traits

--- src/dense/array/operators/scalar_mult.rs
+++ src/dense/array/operators/scalar_mult.rs
@@ -1,118 +1,78 @@
 //! Container representing multiplication with a scalar
 
-use crate::dense::array::{Array, ChunkedAccess, DataChunk, Shape, UnsafeRandomAccessByValue};
-use crate::dense::traits::{UnsafeRandom1DAccessByValue, ValueArrayImpl};
-use crate::dense::types::{c32, c64, RlstNum};
-use crate::RlstScalar;
+use std::ops::Mul;
+
+use crate::dense::array::{Array, Shape, UnsafeRandomAccessByValue};
+use crate::dense::traits::UnsafeRandom1DAccessByValue;
+use crate::dense::types::{c32, c64};
 
 /// Scalar multiplication of array
-pub struct ArrayScalarMult<Item: RlstNum, ArrayImpl: ValueArrayImpl<NDIM, Item>, const NDIM: usize>
-{
-    scalar: Item,
-    operator: Array<Item, ArrayImpl, NDIM>,
+pub struct ArrayScalarMult<Scalar, ArrayImpl, const NDIM: usize> {
+    arr: Array<ArrayImpl, NDIM>,
+    scalar: Scalar,
 }
 
-impl<Item: RlstNum, ArrayImpl: ValueArrayImpl<NDIM, Item>, const NDIM: usize>
-    ArrayScalarMult<Item, ArrayImpl, NDIM>
-{
+impl<Scalar, ArrayImpl, const NDIM: usize> ArrayScalarMult<Scalar, ArrayImpl, NDIM> {
     /// Create new
-    pub fn new(scalar: Item, operator: Array<Item, ArrayImpl, NDIM>) -> Self {
-        Self { scalar, operator }
+    pub fn new(scalar: Scalar, arr: Array<ArrayImpl, NDIM>) -> Self {
+        Self { arr, scalar }
     }
 }
 
-impl<Item: RlstNum, ArrayImpl: ValueArrayImpl<NDIM, Item>, const NDIM: usize>
-    UnsafeRandomAccessByValue<NDIM> for ArrayScalarMult<Item, ArrayImpl, NDIM>
+impl<Scalar: Copy, ArrayImpl, const NDIM: usize> UnsafeRandomAccessByValue<NDIM>
+    for ArrayScalarMult<Scalar, ArrayImpl, NDIM>
+where
+    ArrayImpl: UnsafeRandomAccessByValue<NDIM>,
+    Scalar: Mul<ArrayImpl::Item>,
 {
-    type Item = Item;
-    #[inline]
+    type Item = Scalar::Output;
+
+    #[inline(always)]
     unsafe fn get_value_unchecked(&self, multi_index: [usize; NDIM]) -> Self::Item {
-        self.scalar * self.operator.get_value_unchecked(multi_index)
+        self.scalar * self.arr.get_value_unchecked(multi_index)
     }
 }
 
-impl<Item: RlstNum, ArrayImpl: ValueArrayImpl<NDIM, Item>, const NDIM: usize> Shape<NDIM>
-    for ArrayScalarMult<Item, ArrayImpl, NDIM>
+impl<Scalar: Copy, ArrayImpl, const NDIM: usize> UnsafeRandom1DAccessByValue
+    for ArrayScalarMult<Scalar, ArrayImpl, NDIM>
+where
+    ArrayImpl: UnsafeRandom1DAccessByValue,
+    Scalar: Mul<ArrayImpl::Item>,
 {
-    fn shape(&self) -> [usize; NDIM] {
-        self.operator.shape()
-    }
-}
-
-impl<
-        Item: RlstNum,
-        ArrayImpl: ValueArrayImpl<NDIM, Item> + ChunkedAccess<N, Item = Item>,
-        const NDIM: usize,
-        const N: usize,
-    > ChunkedAccess<N> for ArrayScalarMult<Item, ArrayImpl, NDIM>
-{
-    type Item = Item;
-    #[inline]
-    fn get_chunk(&self, chunk_index: usize) -> Option<DataChunk<Self::Item, N>> {
-        if let Some(mut chunk) = self.operator.get_chunk(chunk_index) {
-            for item in &mut chunk.data {
-                *item *= self.scalar;
-            }
-            Some(chunk)
-        } else {
-            None
-        }
-    }
-}
-
-impl<Item: RlstNum, ArrayImpl: ValueArrayImpl<NDIM, Item>, const NDIM: usize>
-    Array<Item, ArrayImpl, NDIM>
-{
-    /// Multiplication by a scalar
-    pub fn scalar_mul(
-        self,
-        other: Item,
-    ) -> Array<Item, ArrayScalarMult<Item, ArrayImpl, NDIM>, NDIM> {
-        Array::new(ArrayScalarMult::new(other, self))
-    }
-}
-
-impl<Item: RlstScalar, ArrayImpl: ValueArrayImpl<NDIM, Item>, const NDIM: usize> std::ops::Neg
-    for Array<Item, ArrayImpl, NDIM>
-{
-    type Output = Array<Item, ArrayScalarMult<Item, ArrayImpl, NDIM>, NDIM>;
-
-    fn neg(self) -> Self::Output {
-        let minus_one = -<Item as num::One>::one();
-        Array::new(ArrayScalarMult::new(minus_one, self))
-    }
-}
-
-impl<Item: RlstNum, ArrayImpl: ValueArrayImpl<NDIM, Item>, const NDIM: usize>
-    UnsafeRandom1DAccessByValue for ArrayScalarMult<Item, ArrayImpl, NDIM>
-{
-    type Item = Item;
+    type Item = Scalar::Output;
 
     #[inline(always)]
     unsafe fn get_value_1d_unchecked(&self, index: usize) -> Self::Item {
-        self.scalar * self.operator.get_value_1d_unchecked(index)
+        self.scalar * self.arr.get_value_1d_unchecked(index)
+    }
+}
+
+impl<Scalar, ArrayImpl: Shape<NDIM>, const NDIM: usize> Shape<NDIM>
+    for ArrayScalarMult<Scalar, ArrayImpl, NDIM>
+{
+    #[inline(always)]
+    fn shape(&self) -> [usize; NDIM] {
+        self.arr.shape()
+    }
+}
+
+impl<ArrayImpl, const NDIM: usize> Array<ArrayImpl, NDIM> {
+    /// Multiplication by a scalar
+    pub fn scalar_mul<Scalar>(
+        self,
+        other: Scalar,
+    ) -> Array<ArrayScalarMult<Scalar, ArrayImpl, NDIM>, NDIM> {
+        Array::new(ArrayScalarMult::new(other, self))
     }
 }
 
 macro_rules! impl_scalar_mult {
     ($ScalarType:ty) => {
-        impl<ArrayImpl: ValueArrayImpl<NDIM, $ScalarType>, const NDIM: usize>
-            std::ops::Mul<Array<$ScalarType, ArrayImpl, NDIM>> for $ScalarType
-        {
-            type Output = Array<$ScalarType, ArrayScalarMult<$ScalarType, ArrayImpl, NDIM>, NDIM>;
+        impl<ArrayImpl, const NDIM: usize> std::ops::Mul<Array<ArrayImpl, NDIM>> for $ScalarType {
+            type Output = Array<ArrayScalarMult<$ScalarType, ArrayImpl, NDIM>, NDIM>;
 
-            fn mul(self, rhs: Array<$ScalarType, ArrayImpl, NDIM>) -> Self::Output {
+            fn mul(self, rhs: Array<ArrayImpl, NDIM>) -> Self::Output {
                 Array::new(ArrayScalarMult::new(self, rhs))
-            }
-        }
-
-        impl<ArrayImpl: ValueArrayImpl<NDIM, $ScalarType>, const NDIM: usize>
-            std::ops::Mul<$ScalarType> for Array<$ScalarType, ArrayImpl, NDIM>
-        {
-            type Output = Array<$ScalarType, ArrayScalarMult<$ScalarType, ArrayImpl, NDIM>, NDIM>;
-
-            fn mul(self, rhs: $ScalarType) -> Self::Output {
-                rhs * self
             }
         }
     };
--- src/dense/array/operators/subtraction.rs
+++ src/dense/array/operators/subtraction.rs
@@ -1,34 +1,25 @@
 //! Implementation of array addition
 
+use std::ops::Sub;
+
 use crate::dense::{
-    array::{Array, ChunkedAccess, Shape, UnsafeRandomAccessByValue},
-    traits::{UnsafeRandom1DAccessByValue, ValueArrayImpl},
-    types::RlstNum,
+    array::{Array, Shape, UnsafeRandomAccessByValue},
+    traits::UnsafeRandom1DAccessByValue,
 };
 
 /// Subtraction
-pub struct ArraySubtraction<
-    Item: RlstNum,
-    ArrayImpl1: ValueArrayImpl<NDIM, Item>,
-    ArrayImpl2: ValueArrayImpl<NDIM, Item>,
-    const NDIM: usize,
-> {
-    operator1: Array<Item, ArrayImpl1, NDIM>,
-    operator2: Array<Item, ArrayImpl2, NDIM>,
+pub struct ArraySubtraction<ArrayImpl1, ArrayImpl2, const NDIM: usize> {
+    operator1: Array<ArrayImpl1, NDIM>,
+    operator2: Array<ArrayImpl2, NDIM>,
 }
 
-impl<
-        Item: RlstNum,
-        ArrayImpl1: ValueArrayImpl<NDIM, Item>,
-        ArrayImpl2: ValueArrayImpl<NDIM, Item>,
-        const NDIM: usize,
-    > ArraySubtraction<Item, ArrayImpl1, ArrayImpl2, NDIM>
+impl<ArrayImpl1, ArrayImpl2, const NDIM: usize> ArraySubtraction<ArrayImpl1, ArrayImpl2, NDIM>
+where
+    ArrayImpl1: Shape<NDIM>,
+    ArrayImpl2: Shape<NDIM>,
 {
     /// Create new
-    pub fn new(
-        operator1: Array<Item, ArrayImpl1, NDIM>,
-        operator2: Array<Item, ArrayImpl2, NDIM>,
-    ) -> Self {
+    pub fn new(operator1: Array<ArrayImpl1, NDIM>, operator2: Array<ArrayImpl2, NDIM>) -> Self {
         assert_eq!(
             operator1.shape(),
             operator2.shape(),
@@ -43,69 +34,30 @@ impl<
     }
 }
 
-impl<
-        Item: RlstNum,
-        ArrayImpl1: ValueArrayImpl<NDIM, Item>,
-        ArrayImpl2: ValueArrayImpl<NDIM, Item>,
-        const NDIM: usize,
-    > UnsafeRandomAccessByValue<NDIM> for ArraySubtraction<Item, ArrayImpl1, ArrayImpl2, NDIM>
+impl<ArrayImpl1, ArrayImpl2, const NDIM: usize> UnsafeRandomAccessByValue<NDIM>
+    for ArraySubtraction<ArrayImpl1, ArrayImpl2, NDIM>
+where
+    ArrayImpl1: UnsafeRandomAccessByValue<NDIM>,
+    ArrayImpl2: UnsafeRandomAccessByValue<NDIM>,
+    ArrayImpl1::Item: Sub<ArrayImpl2::Item>,
 {
-    type Item = Item;
-    #[inline]
+    type Item = <ArrayImpl1::Item as Sub<ArrayImpl2::Item>>::Output;
+
+    #[inline(always)]
     unsafe fn get_value_unchecked(&self, multi_index: [usize; NDIM]) -> Self::Item {
         self.operator1.get_value_unchecked(multi_index)
             - self.operator2.get_value_unchecked(multi_index)
     }
 }
 
-impl<
-        Item: RlstNum,
-        ArrayImpl1: ValueArrayImpl<NDIM, Item> + ChunkedAccess<N, Item = Item>,
-        ArrayImpl2: ValueArrayImpl<NDIM, Item> + ChunkedAccess<N, Item = Item>,
-        const NDIM: usize,
-        const N: usize,
-    > ChunkedAccess<N> for ArraySubtraction<Item, ArrayImpl1, ArrayImpl2, NDIM>
+impl<ArrayImpl1, ArrayImpl2, const NDIM: usize> UnsafeRandom1DAccessByValue
+    for ArraySubtraction<ArrayImpl1, ArrayImpl2, NDIM>
+where
+    ArrayImpl1: UnsafeRandom1DAccessByValue,
+    ArrayImpl2: UnsafeRandom1DAccessByValue,
+    ArrayImpl1::Item: Sub<ArrayImpl2::Item>,
 {
-    type Item = Item;
-    #[inline]
-    fn get_chunk(
-        &self,
-        chunk_index: usize,
-    ) -> Option<crate::dense::types::DataChunk<Self::Item, N>> {
-        if let (Some(mut chunk1), Some(chunk2)) = (
-            self.operator1.get_chunk(chunk_index),
-            self.operator2.get_chunk(chunk_index),
-        ) {
-            for (elem1, &elem2) in chunk1.data.iter_mut().zip(chunk2.data.iter()) {
-                *elem1 -= elem2;
-            }
-            Some(chunk1)
-        } else {
-            None
-        }
-    }
-}
-
-impl<
-        Item: RlstNum,
-        ArrayImpl1: ValueArrayImpl<NDIM, Item>,
-        ArrayImpl2: ValueArrayImpl<NDIM, Item>,
-        const NDIM: usize,
-    > Shape<NDIM> for ArraySubtraction<Item, ArrayImpl1, ArrayImpl2, NDIM>
-{
-    fn shape(&self) -> [usize; NDIM] {
-        self.operator1.shape()
-    }
-}
-
-impl<
-        Item: RlstNum,
-        ArrayImpl1: ValueArrayImpl<NDIM, Item>,
-        ArrayImpl2: ValueArrayImpl<NDIM, Item>,
-        const NDIM: usize,
-    > UnsafeRandom1DAccessByValue for ArraySubtraction<Item, ArrayImpl1, ArrayImpl2, NDIM>
-{
-    type Item = Item;
+    type Item = <ArrayImpl1::Item as Sub<ArrayImpl2::Item>>::Output;
 
     #[inline(always)]
     unsafe fn get_value_1d_unchecked(&self, index: usize) -> Self::Item {
@@ -113,16 +65,23 @@ impl<
     }
 }
 
-impl<
-        Item: RlstNum,
-        ArrayImpl1: ValueArrayImpl<NDIM, Item>,
-        ArrayImpl2: ValueArrayImpl<NDIM, Item>,
-        const NDIM: usize,
-    > std::ops::Sub<Array<Item, ArrayImpl2, NDIM>> for Array<Item, ArrayImpl1, NDIM>
+impl<ArrayImpl1, ArrayImpl2, const NDIM: usize> Shape<NDIM>
+    for ArraySubtraction<ArrayImpl1, ArrayImpl2, NDIM>
+where
+    ArrayImpl1: Shape<NDIM>,
 {
-    type Output = Array<Item, ArraySubtraction<Item, ArrayImpl1, ArrayImpl2, NDIM>, NDIM>;
+    #[inline(always)]
+    fn shape(&self) -> [usize; NDIM] {
+        self.operator1.shape()
+    }
+}
 
-    fn sub(self, rhs: Array<Item, ArrayImpl2, NDIM>) -> Self::Output {
+impl<ArrayImpl1, ArrayImpl2, const NDIM: usize> std::ops::Sub<Array<ArrayImpl2, NDIM>>
+    for Array<ArrayImpl1, NDIM>
+{
+    type Output = Array<ArraySubtraction<ArrayImpl1, ArrayImpl2, NDIM>, NDIM>;
+
+    fn sub(self, rhs: Array<ArrayImpl2, NDIM>) -> Self::Output {
         Array::new(ArraySubtraction {
             operator1: self,
             operator2: rhs,
