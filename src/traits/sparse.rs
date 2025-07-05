6110ecfc42ee6dc31d43921f31ad13614c6fd52c
Timo Betcke <timo.betcke@gmail.com>
2025-07-05 21:02:29 +0100

    WIP: Sparse mat

---
+++ src/traits/sparse.rs
@@ -0,0 +1,45 @@
+//! Sparse matrix traits
+
+use crate::{
+    dense::array::DynArray, distributed_tools::IndexLayout, sparse::sparse_mat::SparseMatType,
+};
+
+use super::{AijIterator, BaseItem, Shape};
+
+pub trait SparseMat: BaseItem + Shape<2> {
+    /// The type of the index set.
+    type IndexSet;
+
+    /// An iterator that iterates over non-zero elements of the sparse matrix.
+    type AijIter: AijIterator<Item = Self::Item>;
+
+    /// A mutable iterator that iterates over non-zero elements of the sparse matrix.
+    type AijIterMut: AijIterator<Item = Self::Item>;
+
+    /// Get the number of non-zero elements
+    fn nnz(&self) -> usize;
+
+    /// Get the type of the sparse matrix
+    fn sparse_mat_type(&self) -> SparseMatType;
+
+    /// Convert to a dense matrix
+    fn to_dense(&self) -> DynArray<Self::Item, 2>;
+
+    /// Return an iterator over the local non-zero elements in the form (row, column, value).
+    fn iter(&self) -> Self::AijIter;
+}
+
+#[cfg(feature = "mpi")]
+pub trait DistributedSparseMat<'a>: BaseItem + Shape<2> {
+    /// The type of the local space matrix.
+    type LocalSparseMat: SparseMat<Item = Self::Item>;
+
+    /// The type of the communicator.
+    type Comm;
+
+    /// Return the index layout.
+    fn index_layout(&self) -> &IndexLayout<'a, Self::Comm>;
+
+    /// Return the local sparse matrix.
+    fn local(&self) -> &Self::LocalSparseMat;
+}
