c6b1071fe8bb1ad06151334b4c5cc9139fe030d8
Timo Betcke <timo.betcke@gmail.com>
2025-07-31 12:31:34 +0100

    Using typenum for number relationships.

--- rlst/src/base_types.rs
+++ rlst/src/base_types.rs
@@ -3,15 +3,27 @@
 mod number_traits_impl;
 mod rlst_num_impl;
 
+use std::ops::Add;
+
 pub use num::complex::Complex32 as c32;
 pub use num::complex::Complex64 as c64;
 
 use thiserror::Error;
+use typenum::Cmp;
+use typenum::Const;
+use typenum::Less;
+use typenum::NonZero;
+use typenum::PInt;
+use typenum::ToUInt;
+use typenum::Unsigned;
+use typenum::U;
 
 use crate::traits::number_relations::{IsGreaterByOne, IsGreaterZero, IsSmallerByOne};
 use crate::ContainerType;
 use crate::ContainerTypeSelector;
 use crate::IsSmallerThan;
+
+use typenum;
 
 /// The Rlst error type.
 #[derive(Error, Debug)]
@@ -121,95 +133,41 @@ pub type LapackResult<T> = std::result::Result<T, LapackError>;
 /// Empty type for number relations.
 pub struct NumberType<const N: usize>;
 
-impl IsGreaterByOne<0> for NumberType<1> {}
-impl IsGreaterByOne<1> for NumberType<2> {}
-impl IsGreaterByOne<2> for NumberType<3> {}
-impl IsGreaterByOne<3> for NumberType<4> {}
-impl IsGreaterByOne<4> for NumberType<5> {}
-impl IsGreaterByOne<5> for NumberType<6> {}
-impl IsGreaterByOne<6> for NumberType<7> {}
-impl IsGreaterByOne<7> for NumberType<8> {}
-impl IsGreaterByOne<8> for NumberType<9> {}
-impl IsGreaterByOne<9> for NumberType<10> {}
-impl IsGreaterByOne<10> for NumberType<11> {}
-impl IsGreaterByOne<11> for NumberType<12> {}
-impl IsGreaterByOne<12> for NumberType<13> {}
-impl IsGreaterByOne<13> for NumberType<14> {}
-impl IsGreaterByOne<14> for NumberType<15> {}
-impl IsGreaterByOne<15> for NumberType<16> {}
-impl IsGreaterByOne<16> for NumberType<17> {}
-impl IsGreaterByOne<17> for NumberType<18> {}
-impl IsGreaterByOne<18> for NumberType<19> {}
-impl IsGreaterByOne<19> for NumberType<20> {}
+// the condition that M = 1 + N.
+impl<const M: usize, const N: usize> IsGreaterByOne<N> for NumberType<M>
+where
+    PInt<U<N>>: Add<typenum::P1, Output = PInt<U<M>>>,
+    Const<N>: ToUInt,
+    Const<M>: ToUInt,
+    <Const<N> as ToUInt>::Output: Unsigned + NonZero,
+    <Const<M> as ToUInt>::Output: Unsigned + NonZero,
+{
+}
 
-impl IsGreaterZero for NumberType<1> {}
-impl IsGreaterZero for NumberType<2> {}
-impl IsGreaterZero for NumberType<3> {}
-impl IsGreaterZero for NumberType<4> {}
-impl IsGreaterZero for NumberType<5> {}
-impl IsGreaterZero for NumberType<6> {}
-impl IsGreaterZero for NumberType<7> {}
-impl IsGreaterZero for NumberType<8> {}
-impl IsGreaterZero for NumberType<9> {}
-impl IsGreaterZero for NumberType<10> {}
-impl IsGreaterZero for NumberType<11> {}
-impl IsGreaterZero for NumberType<12> {}
-impl IsGreaterZero for NumberType<13> {}
-impl IsGreaterZero for NumberType<14> {}
-impl IsGreaterZero for NumberType<15> {}
-impl IsGreaterZero for NumberType<16> {}
-impl IsGreaterZero for NumberType<17> {}
-impl IsGreaterZero for NumberType<18> {}
-impl IsGreaterZero for NumberType<19> {}
-impl IsGreaterZero for NumberType<20> {}
+// the condition that M = N - 1
+impl<const M: usize, const N: usize> IsSmallerByOne<N> for NumberType<M> where
+    NumberType<N>: IsGreaterByOne<M>
+{
+}
 
-impl IsSmallerByOne<1> for NumberType<0> {}
-impl IsSmallerByOne<2> for NumberType<1> {}
-impl IsSmallerByOne<3> for NumberType<2> {}
-impl IsSmallerByOne<4> for NumberType<3> {}
-impl IsSmallerByOne<5> for NumberType<4> {}
-impl IsSmallerByOne<6> for NumberType<5> {}
-impl IsSmallerByOne<7> for NumberType<6> {}
-impl IsSmallerByOne<8> for NumberType<7> {}
-impl IsSmallerByOne<9> for NumberType<8> {}
-impl IsSmallerByOne<10> for NumberType<9> {}
-impl IsSmallerByOne<11> for NumberType<10> {}
-impl IsSmallerByOne<12> for NumberType<11> {}
-impl IsSmallerByOne<13> for NumberType<12> {}
-impl IsSmallerByOne<14> for NumberType<13> {}
-impl IsSmallerByOne<15> for NumberType<14> {}
-impl IsSmallerByOne<16> for NumberType<15> {}
-impl IsSmallerByOne<17> for NumberType<16> {}
-impl IsSmallerByOne<18> for NumberType<17> {}
-impl IsSmallerByOne<19> for NumberType<18> {}
-impl IsSmallerByOne<20> for NumberType<19> {}
+// Implement the condition that N is not zero.
+impl<const N: usize> IsGreaterZero for NumberType<N>
+where
+    Const<N>: ToUInt,
+    <Const<N> as ToUInt>::Output: NonZero,
+{
+}
 
-impl IsSmallerThan<1> for NumberType<0> {}
-
-impl IsSmallerThan<2> for NumberType<0> {}
-impl IsSmallerThan<2> for NumberType<1> {}
-
-impl IsSmallerThan<3> for NumberType<0> {}
-impl IsSmallerThan<3> for NumberType<1> {}
-impl IsSmallerThan<3> for NumberType<2> {}
-
-impl IsSmallerThan<4> for NumberType<0> {}
-impl IsSmallerThan<4> for NumberType<1> {}
-impl IsSmallerThan<4> for NumberType<2> {}
-impl IsSmallerThan<4> for NumberType<3> {}
-
-impl IsSmallerThan<5> for NumberType<0> {}
-impl IsSmallerThan<5> for NumberType<1> {}
-impl IsSmallerThan<5> for NumberType<2> {}
-impl IsSmallerThan<5> for NumberType<3> {}
-impl IsSmallerThan<5> for NumberType<4> {}
-
-impl IsSmallerThan<6> for NumberType<0> {}
-impl IsSmallerThan<6> for NumberType<1> {}
-impl IsSmallerThan<6> for NumberType<2> {}
-impl IsSmallerThan<6> for NumberType<3> {}
-impl IsSmallerThan<6> for NumberType<4> {}
-impl IsSmallerThan<6> for NumberType<5> {}
+// Implement the condition that M < N.
+impl<const M: usize, const N: usize> IsSmallerThan<N> for NumberType<M>
+where
+    Const<N>: ToUInt,
+    Const<M>: ToUInt,
+    <Const<N> as ToUInt>::Output: NonZero + Unsigned,
+    <Const<M> as ToUInt>::Output: NonZero + Unsigned,
+    PInt<U<M>>: Cmp<PInt<U<N>>, Output = Less>,
+{
+}
 
 /// Determine whether a matrix is upper or lower triangular.
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
