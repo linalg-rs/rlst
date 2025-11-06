cdb1dbc9d17b61487da329acefa31c51054f6046
Timo Betcke <timo.betcke@gmail.com>
2025-11-06 13:26:59 +0000

    Added default traits for typical use cases.

--- rlst/src/traits.rs
+++ rlst/src/traits.rs
@@ -28,3 +28,70 @@ pub use number_relations::*;
 pub use number_traits::*;
 pub use rlst_num::*;
 pub use sparse::*;
+
+/// Default trait for Arrays that can return values.
+pub trait ValueArrayImpl<Item, const NDIM: usize>:
+    UnsafeRandom1DAccessByValue<Item = Item>
+    + UnsafeRandomAccessByValue<NDIM, Item = Item>
+    + Shape<NDIM>
+{
+}
+
+impl<A, Item, const NDIM: usize> ValueArrayImpl<Item, NDIM> for A where
+    A: UnsafeRandom1DAccessByValue<Item = Item>
+        + UnsafeRandomAccessByValue<NDIM, Item = Item>
+        + Shape<NDIM>
+{
+}
+
+/// Default trait for Arrays that can return references.
+pub trait RefArrayImpl<Item, const NDIM: usize>:
+    ValueArrayImpl<Item, NDIM>
+    + UnsafeRandom1DAccessByRef<Item = Item>
+    + UnsafeRandomAccessByRef<NDIM, Item = Item>
+{
+}
+
+impl<A, Item, const NDIM: usize> RefArrayImpl<Item, NDIM> for A where
+    A: ValueArrayImpl<Item, NDIM>
+        + UnsafeRandom1DAccessByRef<Item = Item>
+        + UnsafeRandomAccessByRef<NDIM, Item = Item>
+{
+}
+
+/// Default trait for mutable arrays.
+pub trait MutableArrayImpl<Item, const NDIM: usize>:
+    RefArrayImpl<Item, NDIM>
+    + UnsafeRandom1DAccessMut<Item = Item>
+    + UnsafeRandomAccessMut<NDIM, Item = Item>
+{
+}
+
+impl<A, Item, const NDIM: usize> MutableArrayImpl<Item, NDIM> for A where
+    A: RefArrayImpl<Item, NDIM>
+        + UnsafeRandom1DAccessMut<Item = Item>
+        + UnsafeRandomAccessMut<NDIM, Item = Item>
+{
+}
+
+/// Default trait for arrays that allow RawAccess.
+pub trait RawAccessArrayImpl<Item, const NDIM: usize>:
+    ValueArrayImpl<Item, NDIM> + RawAccess<Item = Item> + Stride<NDIM>
+{
+}
+
+impl<A, Item, const NDIM: usize> RawAccessArrayImpl<Item, NDIM> for A where
+    A: ValueArrayImpl<Item, NDIM> + RawAccess<Item = Item> + Stride<NDIM>
+{
+}
+
+/// Default trait for arrays that allow mutable raw access.
+pub trait MutableRawAccessArrayImpl<Item, const NDIM: usize>:
+    RawAccessArrayImpl<Item, NDIM> + RawAccessMut<Item = Item>
+{
+}
+
+impl<A, Item, const NDIM: usize> MutableRawAccessArrayImpl<Item, NDIM> for A where
+    A: RawAccessArrayImpl<Item, NDIM> + RawAccessMut<Item = Item>
+{
+}
