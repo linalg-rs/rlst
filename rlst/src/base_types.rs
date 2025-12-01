2e57b728be277f982e32a4fff814bf851ae84f2c
Timo Betcke <timo.betcke@gmail.com>
2025-12-01 09:41:15 +0000

    Fixed formatting

--- rlst/src/base_types.rs
+++ rlst/src/base_types.rs
@@ -217,4 +217,3 @@ impl<const N: usize> ContainerTypeSelector<Unknown, Stack<N>> for SelectContaine
 impl<const N: usize> ContainerTypeSelector<Stack<N>, Stack<N>> for SelectContainerType {
     type Type = Stack<N>;
 }
-
